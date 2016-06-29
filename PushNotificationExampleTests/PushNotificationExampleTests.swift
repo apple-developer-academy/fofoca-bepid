29e7740349810402b9d7e5f0e3ab90637d0e8f69
Murilo Da Paixão Lima <murilop@imac-de-bepid-4.local>
2016-06-29 14:52:58 -0300

    Initial Commit

---
+++ PushNotificationExampleTests/PushNotificationExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  PushNotificationExampleTests.swift
+//  PushNotificationExampleTests
+//
+//  Created by Murilo Da Paixão Lima on 29/06/16.
+//  Copyright © 2016 Murilo Da Paixão Lima. All rights reserved.
+//
+
+import XCTest
+@testable import PushNotificationExample
+
+class PushNotificationExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
